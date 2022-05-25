126855c9cc2621631be5b17beffdc0ca27d4e70e
thomas <tomas_hormazabal@hotmail.com>
2022-05-24 22:43:23 -0400

    avnce comentarios

--- ProyectoTecMoviles/ProyectoFlutter/lib/widgets/card_slider.dart
+++ ProyectoTecMoviles/ProyectoFlutter/lib/widgets/card_slider.dart
@@ -5,7 +5,7 @@ class CardSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 200,
+      height: 250,
       color: Colors.red,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -15,22 +15,44 @@ class CardSlider extends StatelessWidget {
             child: Text('titulo',
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
           ),
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (_, int index) {
-                return Container(
-                  width: 130,
-                  height: 190,
-                  color: Colors.green,
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                );
-              },
-            ),
-          ),
+          _SliderBajo(),
+          //Expanded(
+          //child: ListView.builder(
+          //scrollDirection: Axis.horizontal,
+          //itemCount: 5,
+          //itemBuilder: (_, int index) {
+          //return _SliderBajo();
+          //},
+          //),
+          //),
         ],
       ),
     );
+  }
+}
+
+class _SliderBajo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 500,
+      height: 190,
+      color: Colors.green,
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(children: [
+        Text(
+          'Infojksdjlksdjfklsdjfsldjfkslfjlsjfskljfskljfsjsjdfjsdkfjlskdfjijflskjefisdlfjsieofjosiejfiosejflsejflk',
+          maxLines: 5,
+          overflow: TextOverflow.ellipsis,
+        )
+      ]),
+    );
+  }
+}
+
+class _Comented extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
