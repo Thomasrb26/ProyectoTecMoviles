0310ccd460b5c76bbba719e08fa390974ca3793b
Marcelo <97marcelo.mg@gmail.com>
2022-06-22 20:03:02 -0400

    Avance Vista detalles 1.1

--- ProyectoTecMoviles/ProyectoFlutter/lib/screens/product_screen.dart
+++ ProyectoTecMoviles/ProyectoFlutter/lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/screens/ui/input_decorations.dart';
 import 'package:flutter_demo/widgets/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -11,11 +12,83 @@ class ProductScreen extends StatelessWidget {
             Stack(
               children: [
                 ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    onPressed: () => Navigator.pushNamed(context, 'home'),
+                    icon: Icon(Icons.arrow_back_ios_new,
+                        size: 40, color: Colors.white),
+                  ),
+                ),
+                Positioned(
+                  top: 60,
+                  right: 20,
+                  child: IconButton(
+                    onPressed: () {
+                      //TODO camara o galeria
+                    },
+                    icon: Icon(Icons.camera_alt_outlined,
+                        size: 40, color: Colors.white),
+                  ),
+                )
               ],
-            )
+            ),
+            _ActividadForm(),
+            SizedBox(height: 100),
           ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save_outlined),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class _ActividadForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: 'nombre actividad',
+                  labelText: 'Nombre',
+                ),
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: 'descripcion actividad',
+                  labelText: 'Descripcion',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                offset: Offset(0, 5),
+                blurRadius: 5)
+          ]);
 }
